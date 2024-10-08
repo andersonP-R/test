26b07a9f25e92227b3fc4c89eba1e6bafd197c65
Anderson Rivera <anderson.pr.dev@gmail.com>
2024-10-08 18:29:43 -0500

    fixes 3

--- src/app/dashboard/historia-clinica/page.tsx
+++ src/app/dashboard/historia-clinica/page.tsx
@@ -22,98 +22,94 @@ export default async function HistoriaPage() {
         </Link>
       </div>
 
-      {histories.length ? (
-        histories.map((h) => (
-          <div
-            className="flex flex-col border-2 border-black p-2 mb-4"
-            key={h.id}
-          >
-            <span>
-              {" "}
-              <strong>ID historia clinica:</strong> {h.id}
-            </span>
+      {histories.map((h) => (
+        <div
+          className="flex flex-col border-2 border-black p-2 mb-4"
+          key={h.id}
+        >
+          <span>
+            {" "}
+            <strong>ID historia clinica:</strong> {h.id}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Nombre:</strong> {h.nombre} {h.apellido}
-            </span>
+          <span>
+            {" "}
+            <strong>Nombre:</strong> {h.nombre} {h.apellido}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Edad:</strong> {h.edad}
-            </span>
+          <span>
+            {" "}
+            <strong>Edad:</strong> {h.edad}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Estado civil:</strong> {h.estadoCivil}
-            </span>
+          <span>
+            {" "}
+            <strong>Estado civil:</strong> {h.estadoCivil}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Genero:</strong> {h.genero}
-            </span>
+          <span>
+            {" "}
+            <strong>Genero:</strong> {h.genero}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Ocupación:</strong> {h.ocupacion}
-            </span>
+          <span>
+            {" "}
+            <strong>Ocupación:</strong> {h.ocupacion}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Teléfono:</strong> {h.telefono}
-            </span>
+          <span>
+            {" "}
+            <strong>Teléfono:</strong> {h.telefono}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Fecha nacimiento:</strong> {h.fechaNacimiento}
-            </span>
+          <span>
+            {" "}
+            <strong>Fecha nacimiento:</strong> {h.fechaNacimiento}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Direccion:</strong> {h.direccion}
-            </span>
+          <span>
+            {" "}
+            <strong>Direccion:</strong> {h.direccion}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Antecedentes:</strong> {h.antecedentes}
-            </span>
+          <span>
+            {" "}
+            <strong>Antecedentes:</strong> {h.antecedentes}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Diagnostico:</strong> {h.diagnosticos}
-            </span>
+          <span>
+            {" "}
+            <strong>Diagnostico:</strong> {h.diagnosticos}
+          </span>
 
-            <span>
-              {" "}
-              <strong>Tratamientos:</strong> {h.tratamientos}
-            </span>
+          <span>
+            {" "}
+            <strong>Tratamientos:</strong> {h.tratamientos}
+          </span>
 
-            <span className="border-b-2 border-black">
-              {" "}
-              <strong>Notas Medicas:</strong> {h.notasMedicas}
-            </span>
+          <span className="border-b-2 border-black">
+            {" "}
+            <strong>Notas Medicas:</strong> {h.notasMedicas}
+          </span>
 
-            <span className="mt-4">Acciones:</span>
-            <div className="flex">
-              <Link
-                href="/dashboard/citas-medicas"
-                className="border-2 border-black p-2 mr-2"
-              >
-                Solicitar cita medica
-              </Link>
+          <span className="mt-4">Acciones:</span>
+          <div className="flex">
+            <Link
+              href="/dashboard/citas-medicas"
+              className="border-2 border-black p-2 mr-2"
+            >
+              Solicitar cita medica
+            </Link>
 
-              <Link href="#" className="border-2 border-black p-2 mr-2">
-                Actualizar
-              </Link>
-              <Link href="#" className="border-2 border-black p-2 mr-2">
-                Borrar
-              </Link>
-            </div>
+            <Link href="#" className="border-2 border-black p-2 mr-2">
+              Actualizar
+            </Link>
+            <Link href="#" className="border-2 border-black p-2 mr-2">
+              Borrar
+            </Link>
           </div>
-        ))
-      ) : (
-        <div>Nothin here</div>
-      )}
+        </div>
+      ))}
       <div></div>
     </div>
   );
